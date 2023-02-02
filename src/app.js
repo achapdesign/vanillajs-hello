2c36ac6706d03e6821dfd83a697ee0ddda6f443a
achapdesign <achapdesign@gmail.com>
2023-02-02 19:07:27 +0000

    more domain possibilities

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "his", "her", "your", "their"];
+let adj = ["great", "big", "small"];
+let noun = ["cat", "dog", "donkey", "horse"];
+let extension = [
+  ".com",
+  ".net",
+  ".us",
+  ".pt",
+  ".es",
+  "de",
+  ".io",
+  ".fr",
+  ".cz",
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+      }
+    }
+  }
+}
